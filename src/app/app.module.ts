da1829be70f7729715d35c2d68993ec02450d850
Mout Pessemier <mout.pessemier@student.hogent.be>
2019-02-26 20:43:39 +0100

    [Feat] Frist component template setup v1.00

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import {
+  MatCardModule,
+  MatIconModule,
+  MatButtonModule
+} from '@angular/material';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, CarouselComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
