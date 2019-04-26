0b97a2683d6240d89ef69625d07002bc23a0b203
Mout Pessemier <mout.pessemier@student.hogent.be>
2019-04-26 22:56:09 +0200

    [Feat] Fixed routing and video component + resolver, added the safe pipe, updated image component V1.21 Todo: - subrouting for image component - register & login - fixed footer - work out comment component and seperate specs into component

--- src/app/video/video.module.ts
+++ src/app/video/video.module.ts
@@ -5,17 +5,18 @@ import { VideoComponent } from './video/video.component';
 import { MaterialModule } from '../material/material.module';
 import { Routes, RouterModule } from '@angular/router';
 import { VideoResolver } from './video.resolver';
+import { SafePipe } from './safe.pipe';
 
 const appRoutes: Routes = [
   {
-    path: '',
+    path: 'videos',
     component: CarouselComponent,
     resolve: { video: VideoResolver }
   }
 ];
 
 @NgModule({
-  declarations: [CarouselComponent, VideoComponent],
+  declarations: [CarouselComponent, VideoComponent, SafePipe],
   imports: [CommonModule, MaterialModule, RouterModule.forChild(appRoutes)]
 })
 export class VideoModule {}
